03673ac0648d0df90b9004c803d59008cf934c8a
Capaldo Gennaro (EXT REPLY) <g.capaldo@reply.it>
2026-04-22 18:13:25 +0200

    added lombok

--- src/main/java/com/example/demo/dto/UserRequest.java
+++ src/main/java/com/example/demo/dto/UserRequest.java
@@ -1,19 +1,14 @@
 package com.example.demo.dto;
 
 import com.example.demo.model.Role;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserRequest {
 
     private String name;
     private String surname;
     private Role role;
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getSurname() { return surname; }
-    public void setSurname(String surname) { this.surname = surname; }
-
-    public Role getRole() { return role; }
-    public void setRole(Role role) { this.role = role; }
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,8 +1,13 @@
 package com.example.demo.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.UUID;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
@@ -21,16 +26,4 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
-
-    public UUID getId() { return id; }
-    public void setId(UUID id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getSurname() { return surname; }
-    public void setSurname(String surname) { this.surname = surname; }
-
-    public Role getRole() { return role; }
-    public void setRole(Role role) { this.role = role; }
 }
